e3995b7b39f50259cc0c726f7bc59176693d1718
Joshua Salceda <joshuasalceda0021@gmail.com>
2024-04-11 06:55:05 +0800

    Committing minor changes

--- BPLMM/models.py
+++ BPLMM/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 class ACR_GROUPS(models.Model):
     ACR_GROUPID = models.CharField(max_length=255, primary_key=True)
