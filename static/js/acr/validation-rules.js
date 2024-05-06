9c573fe610bf1f5d81acb27be0d3bfd353ea7a41
Joshua Salceda <joshuasalceda0021@gmail.com>
2024-05-06 15:24:58 +0800

    UI: Major UI changes

--- static/js/acr/validation-rules.js
+++ static/js/acr/validation-rules.js
@@ -1,19 +1,23 @@
 var textarea = document.querySelector('#textarea');
-
 var buttons = document.querySelectorAll('.key');
+var cursorPos = 0;
+
+textarea.addEventListener('blur', function() {
+    cursorPos = textarea.selectionStart;
+});
 
 buttons.forEach(function(button) {
     button.addEventListener('click', function() {
         var text = this.querySelector('span').textContent;
 
-        var cursorPos = textarea.selectionStart;
-
         var currentValue = textarea.value;
         var newValue = currentValue.substring(0, cursorPos) + text + currentValue.substring(cursorPos);
         textarea.value = newValue;
 
-        textarea.selectionStart = cursorPos + text.length;
-        textarea.selectionEnd = cursorPos + text.length;
+        cursorPos += text.length;
+        textarea.selectionStart = cursorPos;
+        textarea.selectionEnd = cursorPos;
         this.blur();
+        textarea.focus();
     });
 });
